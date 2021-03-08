234b67404f7c96251f4fa825d07ef9cd5c914b2b
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 23:12:30 -0800

    added test to check that years divisible by 400 are returned as leap years

--- test_leap_year.py
+++ test_leap_year.py
@@ -7,8 +7,10 @@ class TestLeapYear(unittest.TestCase):
     
         year1 = 4
         year2 = 100
+        year3 = 400
         self.assertEqual(leap_year.leap_year(year1), str(year1) + " is a leap year. ")
         self.assertEqual(leap_year.leap_year(year2), str(year2) + " is not a leap year. ")
+        self.assertEqual(leap_year.leap_year(year3), str(year3) + " is a leap year. ")
 
 
 if __name__ == '__main__':
